0dcbc08cb3b44e913dc9362b59e55b984713cd70
abhishekdtpl039 <113986394+abhishekdtpl039@users.noreply.github.com>
2024-01-12 17:42:12 +0530

    seeder for society and users done

--- database/seeders/MasterusersSeeder.php
+++ database/seeders/MasterusersSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Admin\MasterUser;
+use App\Models\Master\MasterSociety;
 use Carbon\Carbon;
 
 
@@ -19,23 +20,41 @@ class MasterusersSeeder extends Seeder
     public function run()
     {
         $obj = new MasterUser();
-        // $ins_arr['society_unique_code'] = $obj->generateSocietyCode();
-        
         MasterUser::create([
             'name'              => 'Super Admin',
-            'user_code'       =>$obj->generateUserCode(),
-            'username'          => 'admin001',
-            'email'             => 'superadmin@dahlia.com',           
+            'user_code'       => $obj->generateUserCode(),
+            'username'          => 'superadmin001',
+            'email'             => 'superadmin@dahlia.com',
             'phone_number'      => '9988998899',
             'gender'            => 'Male',
-            'password'          => Hash::make('admin@123'),           
-            'usertype'          =>2,
-            'country_id'           =>101,
-            'state_id'             =>12,
-            'city_id'              =>5,
-            'updated_at'       =>Carbon::now()->format('Y-m-d H:i:s'),
-            'created_at'      =>Carbon::now()->format('Y-m-d H:i:s')
-            //'email_verified_at' =>Carbon::now()->format('Y-m-d H:i:s')
+            'password'          => Hash::make('admin@123'),
+            'usertype'          => 2,
+            'country_id'           => 101,
+            'state_id'             => 12,
+            'city_id'              => 5,
+            'updated_at'       => Carbon::now()->format('Y-m-d H:i:s'),
+            'created_at'      => Carbon::now()->format('Y-m-d H:i:s')
+
+
+        ]);
+        $data = MasterSociety::get();
+        $data_query = $data->first()->toArray()['id'];
+        MasterUser::create([
+            'name'              => 'Admin',
+            'user_code'       => $obj->generateUserCode(),
+            'username'          => 'admin002',
+            'email'             => 'admin@dahlia.com',
+            'phone_number'      => '9988998898',
+            'gender'            => 'Male',
+            'master_society_ids' => jsonEncodeIntArr([$data_query]),
+            'password'          => Hash::make('admin@123'),
+            'usertype'          => 2,
+            'country_id'           => 101,
+            'state_id'             => 12,
+            'city_id'              => 5,
+            'updated_at'       => Carbon::now()->format('Y-m-d H:i:s'),
+            'created_at'      => Carbon::now()->format('Y-m-d H:i:s')
+
 
         ]);
     }
