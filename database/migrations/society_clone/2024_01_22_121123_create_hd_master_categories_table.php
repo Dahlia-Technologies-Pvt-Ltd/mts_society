479b3b3251e51f10ecb75b2430eefa193c2fb9ce
priti-dt <103584553+priti-dt@users.noreply.github.com>
2024-01-22 18:10:26 +0530

    changes in category migration files

--- database/migrations/society_clone/2024_01_22_121123_create_hd_master_categories_table.php
+++ database/migrations/society_clone/2024_01_22_121123_create_hd_master_categories_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('hd_master_sub_categories_table', function (Blueprint $table) {
+        Schema::create('hd_master_categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained();//no need to society id - we can get this from category table
-            $table->string('sub_category_name')->nullable()->default(null);
+            $table->foreignId('societies_id')->constrained();
+            $table->string('category_name')->nullable()->default(null);
             $table->string('image')->nullable()->default(null); 
             $table->tinyInteger('status')->default(0)->comment('0-Active,1-Inactive');//)0-Active, 1-Means Inactive                  
             $table->unsignedBigInteger('created_by')->nullable();
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('hd_master_sub_categories_table');
+        Schema::dropIfExists('hd_master_categories');
     }
 };
--- database/migrations/society_clone/2024_01_22_121634_create_hd_master_category_resolvers_table_table.php
+++ database/migrations/society_clone/2024_01_22_121634_create_hd_master_category_resolvers_table_table.php
@@ -13,9 +13,8 @@ return new class extends Migration
     {
         Schema::create('hd_master_category_resolvers_table', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained();//no need to society id - we can get this from category table
-            $table->foreignId('sub_category_id')->constrained()->default(0)->nullable();//we are not using this right now
-          
+            $table->foreignId('hd_master_category_id')->constrained();//no need to society id - we can get this from category table
+            $table->foreignId('hd_master_sub_category_id')->constrained()->default(0)->nullable();//we are not using this right now
             $table->foreignId('non_residential_user_detail_id')->constrained()->default(0)->nullable();  //Resolver*
             $table->string('service_provider_master_ids')->nullable()->default(null); //array - Service Provided by resolver
 
