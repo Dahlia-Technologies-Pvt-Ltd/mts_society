767417394930dd61cf4744d3544ac78e06a26259
abhishekdtpl039 <113986394+abhishekdtpl039@users.noreply.github.com>
2024-01-29 15:36:10 +0530

    code cleaned up

--- app/Http/Controllers/API/Admin/FlatController.php
+++ app/Http/Controllers/API/Admin/FlatController.php
@@ -5,17 +5,42 @@ namespace App\Http\Controllers\API\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\API\ResponseController;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Admin\{Flat,Floor};
 
-class FlatController extends Controller
+class FlatController extends ResponseController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    function list_show_query()
     {
-        //
+        $data_query = Flat::join('floors', 'floors.id', '=', 'flats.floor_id')
+        ->join('towers', 'towers.id', '=', 'floors.tower_id')->Leftjoin('wings', 'wings.id', '=', 'floors.wing_id')
+        ->join('users', 'users.id', '=', 'flats.user_id');
+        $data_query->select([
+            'flats.id AS id',
+            'towers.tower_name AS tower_name',
+            'wings.wings_name',
+            'flats.flat_name AS flat_name',
+            'floors.id AS floor_id',
+            'floors.floor_name AS floor_name',
+            'user_id',
+            'users.full_name AS full_name',
+            'users.username AS username',
+            'users.email AS email',
+            'users.zipcode AS zipcode'
+            // 'towers.tower_name AS tower_name',
+            // 
+            
+        ]);
+        return $data_query;
+    }
+    public function indexing(Request $request)
+    {
+        $data_query = $this->list_show_query();
+        print_r($data_query->get()->toArray());die(); //
     }
 
     /**
@@ -25,15 +50,6 @@ class FlatController extends Controller
     {
        
         $societies_id = getsocietyid($request->header('society_id'));
-        $sql = "SELECT id,master_society_ids FROM users where master_society_ids= $societies_id";
-        $results = DB::select($sql);
-        $net_id=$results[0]->master_society_ids;
-        print_r($net_id);die();
-        $societies_id_json = json_encode($societies_id);
-        $sql = "SELECT id FROM users WHERE JSON_CONTAINS_ANY(master_society_id, '$societies_id_json')";
-        $sql = "SELECT id FROM users WHERE master_society_id =  $societies_id";
-        $results = DB::select($sql);
-        $net_id=$results[0]->id;
         if ($request->id > 0) {
             $existingRecord = Flat::find($request->id);
             if (!$existingRecord) {
@@ -52,7 +68,8 @@ class FlatController extends Controller
         }
         $id = empty($request->id) ? 'NULL' : $request->id;
         $validator = Validator::make($request->all(), [
-            'flat_name'                                    => 'required|unique:towers,tower_name,' . $id . ',id,deleted_at,NULL|max:255',
+            'flat_name'                                    => 'required|unique:flats,flat_name,' . $id . ',id,deleted_at,NULL|max:255',
+            'floor_id'      =>'required|integer|min:1'
             
             
         ]);
@@ -61,20 +78,9 @@ class FlatController extends Controller
             return $this->validatorError($validator);
         } else {
             $message = empty($request->id) ? "Flat created successfully." : "Flat updated successfully.";
-            // ,[flat_name]
-            // ,[user_id]
-            // ,[floor_id]
-            // ,[status]
-            // ,[created_by]
-            // ,[updated_by]
-            // ,[deleted_at]
-            // ,[created_at]
-            // ,[updated_at]
-
             $ins_arr = [
-                'societies_id'                        => $net_id,
-                'flat_name'                     => $request->tower_name,
-                'user_id'                     => $request->user_id,
+                'flat_name'                     => $request->flat_name,
+                // 'user_id'                     =>  auth()->id(),
                 'floor_id'                     => $request->floor_id,
                 'updated_by'                           => auth()->id(),
             ];
@@ -92,8 +98,8 @@ class FlatController extends Controller
             if ($qry) {
                 $response['status'] = 200;
                 $response['message'] = $message;
-                $response['data'] = ['id' => $qry->id,'child_society_id'=>$net_id,'master_societies_id' =>$societies_id , 
-                'tower_name' => $qry->tower_name];
+                $response['data'] = ['id' => $qry->id,'user_id'=>$net_id,'floor_id' =>$qry->floor_id, 
+                'flat_name' => $qry->flat_name];
                 return $this->sendResponse($response);
             } else {
                 $response['status'] = 400;
@@ -106,7 +112,7 @@ class FlatController extends Controller
                 $response['status'] = 200;
                 return $this->sendResponse($response);
             }
-            $response['message'] = 'Unable to save tower.';
+            $response['message'] = 'Unable to save flat.';
             $response['status'] = 400;
             return $this->sendError($response);
         }
@@ -131,7 +137,7 @@ class FlatController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function delete(string $id)
     {
         //
     }
