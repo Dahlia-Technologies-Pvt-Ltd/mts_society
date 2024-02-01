0ff845bb127e3187c583c8063cc7982982a94508
abhishekdtpl039 <113986394+abhishekdtpl039@users.noreply.github.com>
2024-02-01 10:53:47 +0530

    parking started

--- app/Http/Controllers/API/Admin/ParkingController.php
+++ app/Http/Controllers/API/Admin/ParkingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Parking;
+use App\Models\Admin\Parking;
 
 class ParkingController extends Controller
 {
@@ -43,8 +43,22 @@ class ParkingController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request)
     {
-        //
+        $terms = Parking::find($request->id);
+        if ($terms) {
+            $ins_arr['deleted_by'] = auth()->id();
+            $qry = Parking::updateOrCreate(
+                ['id' => $request->id],
+                $ins_arr
+            );
+            $terms->destroy($request->id);
+            $message = "Record Deleted Successfully !";
+        } else {
+            $message = "Record Not Found !";
+        }
+        $response['message'] = $message;
+        $response['status'] = 200;
+        return $this->sendResponse($response);
     }
 }
--- config/util.php
+++ config/util.php
@@ -5,4 +5,9 @@ return
         
     'default_date_time_format' => 'd-m-Y h:i a',
     'default_date_format' => 'd-m-Y',
+    'vehichle_type' => [
+        array('name'=>"2 Wheeler",'value'=>"1"),
+        array('name'=>"4 Wheeler",'value'=>"2"),
+        array('name'=>"other vehichles",'value'=>"3"),
+    ]
 ];
